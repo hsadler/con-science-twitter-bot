d4fa3c96b04f57c9f772219f93f9f94abc0c468d
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-06 09:35:56 -0800

    complete test scraper for brainyquote

---
+++ scrape_quotes.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+
+class QuoteScraper:
+
+
+    def scrape_brainy(self):
+
+        scrape_url = 'https://www.brainyquote.com/api/rand_q'
+        res = requests.get(scrape_url)
+
+        new_quote = json.loads(res.text)
+        quotes = None
+
+        with open('test_quotes_scrape.json') as f:
+            quotes = json.load(f)
+
+        quotes.append(new_quote)
+
+        with open('test_quotes_scrape.json', mode='w') as f:
+            json.dump(quotes, f, indent=4)
+
+    print 'brainy scrape complete...'
+
+
+    def print_quote_file(self):
+
+        with open('test_quotes_scrape.json') as f:
+            quotes = json.load(f)
+            pp.pprint(quotes)
+
+
+
+scraper = QuoteScraper()
+scraper.scrape_brainy()
+
+
