96deff650d888f251fd8c20e322351a37ed60479
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-06 23:49:04 -0800

    add quote_scrape controller

---
+++ controllers/quote_scrape.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests
+import json
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
+
+
+class Scraper:
+
+
+    @staticmethod
+    def scrape_brainy():
+        # todo:
+        # make api call
+        # process res data
+        # create quote model instance
+        # store quote
+        pass
+
+
+    @staticmethod
+    def scrape_forismatic():
+        # todo
+        pass
+
+
+    @staticmethod
+    def scrape_stands4():
+        # todo
+        pass
+
+
+
