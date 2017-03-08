f146d348dcf14f2be0e442052654c990d4f294e7
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-08 09:45:37 -0800

    start implementation of quotes class

--- models/quote.py
+++ models/quote.py
@@ -3,6 +3,9 @@
 
 # Quote model
 
+from models.database.database import MySQL_DB
+
+import hashlib
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -11,14 +14,18 @@ pp = pprint.PrettyPrinter(indent=4)
 class Quote:
 
 
-    # todo:
-    # implement CRUD functionality
-
-
-    def __init__(self):
-        # todo:
-        # build constructor
-        pass
+    def __init__(self, quote_text, author, source_url=None, active=1):
+        # CREATE TABLE IF NOT EXISTS quotes(
+        # id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        # quote_text VARCHAR(2000) NOT NULL,
+        # author VARCHAR(255),
+        # source_url VARCHAR(1000),
+        # active TINYINT(1) DEFAULT 1 NOT NULL,
+        # md5hash VARCHAR(32) NOT NULL);
+        self.quote_text = quote_text
+        self.author = author
+        self.source_url = source_url
+        self.active = active
 
 
     def create(self):
