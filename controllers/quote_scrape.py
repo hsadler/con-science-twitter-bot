42bc4a512853e2b678dea6708d37d052a881faae
Harry Sadler <harrypatricksadler@gmail.com>
2017-03-09 19:51:11 -0800

    add notes, add scrape logic to quote_scrape controller

--- controllers/quote_scrape.py
+++ controllers/quote_scrape.py
@@ -3,6 +3,8 @@
 
 # Quote scrape controller
 
+import config
+
 import requests
 import json
 
@@ -11,29 +13,61 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 
-class Scraper:
+class QuoteScrape:
 
 
     @staticmethod
     def scrape_brainy():
-        # todo:
+
         # make api call
+        scrape_url = 'https://www.brainyquote.com/api/rand_q'
+        res = requests.get(scrape_url)
+
+        new_quote = json.loads(res.text)
+        
+        pp.pprint(new_quote)
+        
         # process res data
         # create quote model instance
         # store quote
-        pass
+        
 
 
     @staticmethod
     def scrape_forismatic():
-        # todo
-        pass
+        
+        # make api call
+        scrape_url = 'http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en'
+        res = requests.get(scrape_url)
+
+        new_quote = json.loads(res.text)
+        
+        pp.pprint(new_quote)
+        
+        # process res data
+        # create quote model instance
+        # store quote
 
 
     @staticmethod
     def scrape_stands4():
-        # todo
-        pass
+        
+        # make api call
+        scrape_url = 'http://www.stands4.com/services/v2/quotes.php?uid=[user_id]&tokenid=[user_token]&searchtype=SEARCH'
+        params = {
+            'uid': config.stands4_uid,
+            'tokenid': config.stands4_tokenid,
+            'searchtype': 'SEARCH'
+        }
+        res = requests.get(scrape_url)
+
+        new_quote = json.loads(res.text)
+        
+        pp.pprint(new_quote.__dict__)
+        
+        # process res data
+        # create quote model instance
+        # store quote
 
 
 
--- tests.py
+++ tests.py
@@ -7,16 +7,20 @@ pp = pprint.PrettyPrinter(indent=4)
 
 
 
-try:
-	quote = Quote.create('I am a banana!')
-	quote.save()
-	pp.pprint(quote.__dict__)
-except:
-	print 'insert already exists'
+# try:
+# 	quote = Quote.create('I am a banana!')
+# 	quote.save()
+# 	pp.pprint(quote.__dict__)
+# except:
+# 	print 'insert already exists'
 
 
-fetched_quote = Quote.find_by_quote_text('I am a banana!')
-if fetched_quote is not None:
-    pp.pprint(fetched_quote.__dict__)
+# fetched_quote = Quote.find_by_quote_text('I am a banana!')
+# if fetched_quote is not None:
+#     pp.pprint(fetched_quote.__dict__)
 
 
+from controllers.quote_scrape import QuoteScrape
+
+QuoteScrape.scrape_brainy()
+QuoteScrape.scrape_forismatic()
