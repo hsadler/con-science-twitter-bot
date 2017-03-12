aa4641703bfb2f33370ed5a00c64f210384d96ca
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-11 21:12:41 -0800

    fix bot.py indentation issues

--- bot.py
+++ bot.py
@@ -7,18 +7,17 @@ from controllers.quote_scrape import QuoteScrape
 
 
 def scrape_brainy_and_forismatic():
-
-	try:
-		QuoteScrape.scrape_brainy()
-	except:
+    try:
+        QuoteScrape.scrape_brainy()
+    except:
         ErrorLog.log_exception('brainy scrape fail')
 
-	try:
-		QuoteScrape.scrape_forismatic()
-	except:
+    try:
+        QuoteScrape.scrape_forismatic()
+    except:
         ErrorLog.log_exception('forismatic scrape fail')
 
 
 def batch_scrape_stands4():
 
-	QuoteScrape.batch_scrape_stands4(iterations=100, sleep_time=5)
+    QuoteScrape.batch_scrape_stands4(iterations=100, sleep_time=5)
