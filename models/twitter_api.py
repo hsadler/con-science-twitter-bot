e502636393ef9d9dee771edf98e59c0bdb4981a8
Harry Sadler <harrysadlermusic@gmail.com>
2017-03-19 23:17:31 -0700

    update twitter_api model, add twitter api tests

--- models/twitter_api.py
+++ models/twitter_api.py
@@ -12,11 +12,11 @@ pp = pprint.PrettyPrinter(indent=4)
 class TwitterAPI:
 
     def __init__(self):
-        consumer_key = config.consumer_key
-        consumer_secret = config.consumer_secret
+        consumer_key = config.twitter_consumer_key
+        consumer_secret = config.twitter_consumer_secret
         auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        access_token = config.access_token
-        access_token_secret = config.access_token_secret
+        access_token = config.twitter_access_token
+        access_token_secret = config.twitter_access_token_secret
         auth.set_access_token(access_token, access_token_secret)
         self.api = tweepy.API(auth)
 
@@ -25,25 +25,25 @@ class TwitterAPI:
         self.api.update_status(status=message)
 
 
-    # TODO: These are mostly just testing methods, needs refactor
-
     def print_home_timeline(self):
         public_tweets = self.api.home_timeline()
-        for tweet in public_tweets:
-            print tweet.text
+        return public_tweets
+
+
+    def get_tweets_by_ids(self, ids):
+        tweets = self.api.statuses_lookup(id=ids)
+        return tweets
 
 
     def get_tweets_by_user(self, user):
         user_tweets = self.api.user_timeline(screen_name=user, count=100)
-        for tweet in user_tweets:
-            print tweet.text
+        return user_tweets
 
 
     def get_tweets_by_user_mention(self, user):
         user_handle = '@' + user
         tweets = self.api.search(q=user_handle, count=100)
-        for tweet in tweets:
-            print tweet.text
+        return tweets
 
 
     def get_reply_tweets(self):
--- tests.py
+++ tests.py
@@ -5,7 +5,55 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 
-from controllers.quote_scrape import QuoteScrape
+# from controllers.quote_scrape import QuoteScrape
 
-QuoteScrape.scrape_brainy()
-QuoteScrape.scrape_forismatic()
+# QuoteScrape.scrape_brainy()
+# QuoteScrape.scrape_forismatic()
+
+
+from models.twitter_api import TwitterAPI
+import time
+
+twitter = TwitterAPI()
+
+# print '\n========================================\n'
+
+# users_tweets = twitter.get_tweets_by_user(user='FoxNews')
+# pp.pprint(users_tweets[0].__dict__)
+
+# time.sleep(2)
+
+
+print '\n++++++++++++++++++++++++++++++++++++++++\n'
+
+mention_tweets = twitter.get_tweets_by_user_mention(user='FoxNews')
+
+ids = []
+for tweet in mention_tweets:
+    if tweet.in_reply_to_status_id is not None:
+        print tweet.in_reply_to_status_id
+
+
+# time.sleep(2)
+
+
+# print '\n-----------------------------------------\n'
+
+# orig_tweets = twitter.get_tweets_by_ids(ids=ids)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
